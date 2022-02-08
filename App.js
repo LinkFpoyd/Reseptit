6601b85a4b92993535a1933f3e41fa2fd39c4daa
unknown <leeviva@gmail.com>
2022-02-08 17:10:35 +0200

    toimii

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Alert, Button, Image } from 'react-native';
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [reseptit, setReseptit] = useState([]);
+
+  const getReseptit = () => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${keyword}`)
+    .then(response => response.json())
+    .then(data => setReseptit(data.meals))
+    .catch (error => {
+      Alert.alert('Error', error.toString());
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList style={styles.list}
+        data={reseptit}
+        renderItem={({ item }) => 
+          <View>
+          <Text>{item.strMeal}</Text>
+          <Image style={styles.image} source={{uri: (item.strMealThumb + '/preview')}} />
+          </View> }  
+        keyExtractor={(item, index) => index.toString()}
+      />
+      <TextInput onChangeText={text => setKeyword(text)}
+      value={keyword}
+      placeholder='Hakusana'/>
+      <Button title="Etsi" onPress={getReseptit}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +43,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  list: {
+    marginTop: 100
+  },
+  image: {
+    width: 100,
+    height: 100
+  }
 });
